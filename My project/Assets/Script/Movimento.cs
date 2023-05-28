5cb36d287a89d72b5226f78240d1447e66c93f5d
PolloRequem <dantebaiunco29.06games@gmail.com>
2023-05-28 18:44:07 +0200

    Cambiamenti prova

---
+++ My project/Assets/Script/Movimento.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public int a;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
