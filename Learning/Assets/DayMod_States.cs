640a62b36ecd8a1497b41f002821c10b9ed689ae
PolloRequem <dantebaiunco29.06games@gmail.com>
2023-06-03 01:24:35 +0200

    DayModifier
    Aggiunto il gameobject DayModifier all interono della quale è presente uno script con una variabile GameStates.Inoltre è stato aggiunto uno script ad un Npc per provare i riferimenti al DayMod. Da trovare un modo per rendere pubblico il CurrentGameStates senza usare una referenza nello script

---
+++ Learning/Assets/DayMod_States.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayMod_States : MonoBehaviour
+{
+    public enum GameState
+    {
+        Day1,
+        Day2,
+        Day3,
+        Day4,
+    }
+
+    public GameState currentGameState;
+
+    void Start()
+    {
+        currentGameState = GameState.Day1; 
+        StartDay(); 
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ChangeDay();
+        }
+    }
+
+    void ChangeDay()
+    {
+        switch (currentGameState)
+        {
+            case GameState.Day1:
+                currentGameState = GameState.Day2;
+                break;
+            case GameState.Day2:
+                currentGameState = GameState.Day3;
+                break;
+            case GameState.Day3:  
+                currentGameState= GameState.Day4;
+                break;
+        }
+
+        StartDay(); 
+    }
+
+    void StartDay()
+    {
+       
+        switch (currentGameState)
+        {
+            case GameState.Day1:
+                Debug.Log("Burdell");
+              
+                break;
+            case GameState.Day2:
+                Debug.Log("Porcodio");
+               
+                break;
+            case GameState.Day3:
+                Debug.Log("Modonnaputtana");
+                break;
+
+             case GameState.Day4:
+                Debug.Log("Rovaretti Gay");
+                break;
+        }
+    }
+}
---
+++ Learning/Assets/NpcScrip.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class NPCMaterialChanger : MonoBehaviour
+{
+    public DayMod_States gameManager;
+ 
+
+
+
+    void Update()
+    {
+        if (gameManager != null)
+        {
+            DayMod_States.GameState currentState = gameManager.currentGameState;
+
+           
+            switch (currentState)
+            {
+                case DayMod_States.GameState.Day1:
+                    Debug.Log("suca1");
+                    break;
+                case DayMod_States.GameState.Day2:
+                    Debug.Log("suca2");
+                    break;
+                case DayMod_States.GameState.Day3:
+                    Debug.Log("suca3");
+                    break;
+                   
+            }
+        }
+    }
+}
+
