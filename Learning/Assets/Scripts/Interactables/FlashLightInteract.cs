0a899927090876b4ba330ec785d11a5e3efa75a4
MethodiTick <andrea05muna@gmail.com>
2023-06-04 00:24:34 +0200

    Continuing Flashlight

--- Learning/Assets/Scripts/Interactables/FlashLightInteract.cs
+++ Learning/Assets/Scripts/Interactables/FlashLightInteract.cs
@@ -1,13 +1,25 @@
+using JetBrains.Rider.Unity.Editor;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class FlashLightInteract : Interactable
 {
-    // Start is called before the first frame update
+    public Transform handTransform;
+    public Rigidbody rb;
+    //private bool torciaInMano = false;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     protected override void Interact()
     {
-        
+        transform.SetParent(handTransform);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        Destroy(rb);
+        //torciaInMano = true;
     }
 }
